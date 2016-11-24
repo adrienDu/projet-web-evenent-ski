ac9e05c297f0360661e3a655c26b79173ddff888
Maeva <maeva.mgt@hotmail.fr>
2016-11-24 12:36:15 +0100

    Update form.php

--- form.php
+++ form.php
@@ -1,34 +1,29 @@
 <?php
-echo $_POST['nom'];
-var_dump($_POST);
-var_dump(isset($_POST['nom']));
+var_dump($_GET);
+var_dump(isset($_GET['nom']));
 
-/*if (empty($_POST['nom'])){
-    /*|| empty($_POST['prenom']) || empty($_POST['journais']) || empty($_POST['moisnais'])
-    || empty($_POST['annais']) || empty($_POST['sexe']) || empty($_POST['mail']) || empty($_POST['tel'])
-    || empty($_POST['rue']) || empty($_POST['cp']) || empty($_POST['ville']) || empty($_POST['glisse'])
-    || empty($_POST['pointure']) || empty($_POST['taille']) || empty($_POST['niveau'])) {*/
-  /*  echo "ERREUR : tous les champs n'ont pas ete renseignés.";
+if (empty($_GET['nom']) || empty($_GET['prenom']) || empty($_GET['nais']) || empty($_GET['sexe'])
+    || empty($_GET['mail']) || empty($_GET['tel']) || empty($_GET['rue']) || empty($_GET['cp'])
+    || empty($_GET['ville']) || empty($_GET['glisse']) || empty($_GET['pointure']) || empty($_GET['taille'])
+    || empty($_GET['niveau'])) {
+    echo "ERREUR : tous les champs n'ont pas ete renseignés.";
 
 } else {
     echo "OK";
-   /* $nom = $_POST['nom'];
-    $prenom = $_POST['prenom'];
-    $jnais = $_POST['journais'];
-    $mnais = $_POST['moisnais'];
-    $anais = $_POST['annais'];
-    $sexe = $_POST['sexe'];
-    $mail = $_POST['mail'];
-    $tel = $_POST['tel'];
-    $rue = $_POST['rue'];
-    $cp = $_POST['cp'];
-    $ville = $_POST['ville'];
-    $glisse = $POST['glisse'];
-    $pointure = $_POST['pointure'];
-    $taille = $_POST['taille'];
-    $niveau = $_POST['niveau'];
-   */
+    /* $nom = $_POST['nom'];
+     $prenom = $_POST['prenom'];
+     $nais = $_POST['nais'];
+     $sexe = $_POST['sexe'];
+     $mail = $_POST['mail'];
+     $tel = $_POST['tel'];
+     $rue = $_POST['rue'];
+     $cp = $_POST['cp'];
+     $ville = $_POST['ville'];
+     $glisse = $POST['glisse'];
+     $pointure = $_POST['pointure'];
+     $taille = $_POST['taille'];
+     $niveau = $_POST['niveau'];
+    */
 
-/*}*/
-
+}
 ?>
