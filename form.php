3bb39cf1e5313ec8078f0b5d3500f73d3102366c
adrienDu <adrien.duhoux@gmail.com>
2016-11-28 23:40:35 +0100

    form.php

--- form.php
+++ form.php
@@ -3,6 +3,10 @@
 //var_dump($_GET);
 //var_dump(isset($_GET['nom']));
 $nais = $_GET['nais'];
+$bool = array(0, 1);
+$pointures = array(33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46);
+$taille = array(1.55, 1.60, 1.65, 1.70, 1.75, 1.80, 1.85, 1.9, 1.95, 2.0);
+$niveau = array(0, 1,2);
 
 if (empty($_GET['nom']) || empty($_GET['prenom']) || empty($_GET['nais']) || empty($_GET['sexe'])
     || empty($_GET['mail']) || empty($_GET['tel']) || empty($_GET['rue']) || empty($_GET['cp'])
@@ -19,20 +23,44 @@ if (empty($_GET['nom']) || empty($_GET['prenom']) || empty($_GET['nais']) || emp
         if (!empty($nais)) {
             echo $nais;
         } else {
-            if (!filter_var($_GET['mail'], FILTER_VALIDATE_EMAIL)) {
-                echo "L'adresse mail saisie n'est pas valide";
+            if (!in_array($_get['sexe'], $bool)) {
+                echo "le sexe entré n'est pas proposé";
             } else {
-                if (!preg_match('`[0 - 9]{10}`', $_GET['tel']) || strlen($_GET['tel']) != 10) {
-                    echo "Le numéro de téléphone saisi n'est pas valide";
+                if (!filter_var($_GET['mail'], FILTER_VALIDATE_EMAIL)) {
+                    echo "L'adresse mail saisie n'est pas valide";
                 } else {
-                    if (!preg_match('`[0 - 9] {5}`', $_GET['cp']) || strlen($_GET['cp']) != 5) {
-                        echo "Le code postal saisi n'est pas valide";
+                    if (!preg_match('`[0 - 9]{10}`', $_GET['tel']) || strlen($_GET['tel']) != 10) {
+                        echo "Le numéro de téléphone saisi n'est pas valide";
                     } else {
-                        echo "OK";
+                        if (!preg_match('`[0 - 9] {5}`', $_GET['cp']) || strlen($_GET['cp']) != 5) {
+                            echo "Le code postal saisi n'est pas valide";
+                        } else {
+                            if (!in_array($_GET['glisse'], $bool)) {
+                                echo "ça n'est pas un moyen de glisse";
+                            } else {
+                                if (!in_array($_GET['pointure'], $pointures)) {
+                                    echo "la pointure de chaussure entré n'existe pas";
+                                } else {
+                                    if (!in_array($_GET['taille'], $taille, true)) {
+                                        echo "la taille entré n'existe pas";
+                                    } else {
+                                        if (!in_array($_GET['niveau'], $niveau)) {
+                                            echo "le niveau entré n'existe pas";
+
+                                        } else {
+
+                                        }
+                                    }
+
+                                }
+                                echo "OK";
+                            }
+                        }
                     }
                 }
             }
         }
+
     }
 
     /* $nom = $_POST['nom'];
@@ -55,20 +83,21 @@ if (empty($_GET['nom']) || empty($_GET['prenom']) || empty($_GET['nais']) || emp
 function verifDate($date)
 {
     $dateDiv = explode("-", $date);
-    if (preg_match('#^([0-9]{4})([/-])([0-9]{2})\2([0-9]{2})$#', $date) == 1 && checkdate($dateDiv[1],$dateDiv[2],$dateDiv[0])) {
+    if (preg_match('#^([0-9]{4})([/-])([0-9]{2})\2([0-9]{2})$#', $date) == 1 && checkdate($dateDiv[1], $dateDiv[2], $dateDiv[0])) {
         return verifAge($date);
     } else {
         return "la date saisie n'est pas valide";
     }
 }
-function verifAge($date){
-    if($date[0] > 1998){
+
+function verifAge($date)
+{
+    if ($date[0] > 1998) {
         return "vous êtes trop jeune";
-    }
-    elseif ($date[0] <1986){
+    } elseif ($date[0] < 1986) {
         return "Vous êtes trop vieux";
-    }
-    else return "";
+    } else return "";
 
 }
+
 ?>
