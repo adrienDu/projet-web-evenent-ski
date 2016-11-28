d141d7882463e844b5f9f644ea9f03591c3b4a5a
adrienDu <adrien.duhoux@gmail.com>
2016-11-28 09:44:41 +0100

    test.php

--- testphp.php
+++ testphp.php
@@ -6,6 +6,5 @@
  * Time: 18:18
  */
 require('bdd.php');
-newInsc("Margueritat", "Maeva", 1994-10-31, 1, "maeva.mgt@outlook.com", 0653436645, 3, "rue des mimosa", 75016, "PAris", 1, 42, 168, 2);
 getAll();
 
