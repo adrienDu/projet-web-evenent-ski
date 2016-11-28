5c502189e658790dc80388c3d1d04e621c41c178
Maeva <maeva.mgt@hotmail.fr>
2016-11-28 10:26:05 +0100

    form.php updated 28/11/2016

--- form.php
+++ form.php
@@ -1,29 +1,29 @@
 <?php
-require_once ("bdd.php");
+
 //var_dump($_GET);
 //var_dump(isset($_GET['nom']));
 
 if (empty($_GET['nom']) || empty($_GET['prenom']) || empty($_GET['nais']) || empty($_GET['sexe'])
     || empty($_GET['mail']) || empty($_GET['tel']) || empty($_GET['rue']) || empty($_GET['cp'])
     || empty($_GET['ville']) || empty($_GET['glisse']) || empty($_GET['pointure']) || empty($_GET['taille'])
-    || empty($_GET['niveau'])) {
+    || empty($_GET['niveau'])
+) {
     echo "Tous les champs n'ont pas été renseignés !";
 
 } else {
-    if(strlen($_GET['nom'])>100 || strlen($_GET['prenom'])>100){
+    if (strlen($_GET['nom']) > 100 || strlen($_GET['prenom']) > 100) {
         echo "Les noms ou prenoms saisis ne sont pas valides";
-    }
-    else {
-        if (strlen($_GET['nais']) != 10) {
+    } else {
+        if (strlen($_GET['nais']) != 10 ) {
             echo "La date de naissance saisie n'est pas valide";
         } else {
             if (!filter_var($_GET['mail'], FILTER_VALIDATE_EMAIL)) {
                 echo "L'adresse mail saisie n'est pas valide";
             } else {
-                if (!preg_match('`[0-9]{10}`', $_GET['tel']) || strlen($_GET['tel']) != 10) {
+                if (!preg_match('`[0 - 9]{10}`', $_GET['tel']) || strlen($_GET['tel']) != 10) {
                     echo "Le numéro de téléphone saisi n'est pas valide";
                 } else {
-                    if (!preg_match('`[0-9]{5}`', $_GET['cp']) || strlen($_GET['cp']) != 5) {
+                    if (!preg_match('`[0 - 9] {5}`', $_GET['cp']) || strlen($_GET['cp']) != 5) {
                         echo "Le code postal saisi n'est pas valide";
                     } else {
                         echo "OK";
@@ -32,22 +32,20 @@ if (empty($_GET['nom']) || empty($_GET['prenom']) || empty($_GET['nais']) || emp
             }
         }
     }
-    $nom = $_GET['nom'];
-    $prenom = $_GET['prenom'];
-    $nais = $_GET['nais'];
-    $sexe = $_GET['sexe'];
-    $mail = $_GET['mail'];
-    $tel = $_GET['tel'];
-    $nrue = $_GET['nrue'];
-    $rue = $_GET['rue'];
-    $cp = $_GET['cp'];
-    $ville = $_GET['ville'];
-    $glisse = $_GET['glisse'];
-    $pointure = $_GET['pointure'];
-    $taille = $_GET['taille'];
-    $niveau = $_GET['niveau'];
-
-    newInsc($nom,$prenom,$nais,$sexe,$mail,$tel,$nrue,$rue,$cp,$ville,$glisse,$pointure,$taille,$niveau);
+   /* $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $nais = $_POST['nais'];
+    $sexe = $_POST['sexe'];
+    $mail = $_POST['mail'];
+    $tel = $_POST['tel'];
+    $rue = $_POST['rue'];
+    $cp = $_POST['cp'];
+    $ville = $_POST['ville'];
+    $glisse = $POST['glisse'];
+    $pointure = $_POST['pointure'];
+    $taille = $_POST['taille'];
+    $niveau = $_POST['niveau'];
+   */
 
 }
 
