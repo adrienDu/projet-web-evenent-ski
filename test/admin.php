cb27ab218e076a7d90ac23da5e589a06632a2b8d
Maeva <maeva.mgt@hotmail.fr>
2017-01-01 18:06:58 +0100

    admin

--- test/admin.php
+++ test/admin.php
@@ -23,102 +23,108 @@ include('../head.php');
             else{
             ?>
             <h1 class="title">Utilisateurs en attente</h1>
-            <table class="table">
-                <thead>
-                <tr>
-                    <th class="thstyle">Nom</th>
-                    <th>Prenom</th>
-                    <th>Date de naissance</th>
-                    <th>Sexe</th>
-                    <th>Email</th>
-                    <th>Telephone</th>
-                    <th>Rue</th>
-                    <th>Code postal</th>
-                    <th>Ville</th>
-                    <th>Ski / Snow</th>
-                    <th>Pointure</th>
-                    <th>Taille</th>
-                    <th>Niveau</th>
-                    <th style="display: none">Etat de l'inscription</th>
-                    <th>Date d'inscription</th>
-                    <th>Actions</th>
-                </tr>
-                </thead>
-                <?php
-                while ($donnee = $data->fetch()) {
-                    echo "</tr>";
-                    echo "<td> $donnee[nom] </td>";
-                    echo "<td> $donnee[prenom] </td>";
-                    echo "<td> $donnee[dateNais] </td>";
-                    echo "<td> " . valueSexe($donnee['sexe']) . "</td>";
-                    echo "<td> $donnee[mail] </td>";
-                    echo "<td> $donnee[tel] </td>";
-                    echo "<td> $donnee[rue] </td>";
-                    echo "<td> $donnee[CP] </td>";
-                    echo "<td> $donnee[ville] </td>";
-                    echo "<td>" . valueGlisse($donnee['glisse']) . "</td>";
-                    echo "<td> $donnee[pointure] </td>";
-                    echo "<td> $donnee[taille] </td>";
-                    echo "<td>" . valueNiveau($donnee['niveau']) . "</td>";
-                    echo "<td style='display: none'>" . valueEtatInscr($donnee['etatInscription']) . "</td>";
-                    echo "<td> $donnee[dateInscription]</td>";
-                    echo "<td><a class=\"btn btn-success\" href=\"acceptUser.php?id=\"" . $donnee['idInscript'] . " role=\"button\">Accept</a><a class=\"btn btn-danger\" href=\"refuseUser.php?id=\"" . $donnee['idInscript'] . " role=\"button\">Refuser</a>";
-                    echo "</tr>";
-                    echo "<br />";
-                }
+            <div class="table-responsive">
+                <table class="table table-condensed tablestyle">
+                    <thead class="entete">
+                    <tr>
+                        <th class="thstyle">Nom</th>
+                        <th>Prenom</th>
+                        <th>Date de naissance</th>
+                        <th>Sexe</th>
+                        <th>Email</th>
+                        <th>Telephone</th>
+                        <th>Rue</th>
+                        <th>Code postal</th>
+                        <th>Ville</th>
+                        <th>Ski / Snow</th>
+                        <th>Pointure</th>
+                        <th>Taille</th>
+                        <th>Niveau</th>
+                        <th style="display: none">Etat de l'inscription</th>
+                        <th>Date d'inscription</th>
+                        <th>Actions</th>
+                    </tr>
+                    </thead>
+                    <?php
+                    while ($donnee = $data->fetch()) {
+                        echo "</tr>";
+                        echo "<td class='tdstyle whitefonttdstyle'> $donnee[nom] </td>";
+                        echo "<td class='tdstyle whitefonttdstyle'> $donnee[prenom] </td>";
+                        echo "<td class='tdstyle whitefonttdstyle'> $donnee[dateNais] </td>";
+                        echo "<td class='tdstyle whitefonttdstyle'> " . valueSexe($donnee['sexe']) . "</td>";
+                        echo "<td class='tdstyle whitefonttdstyle'> $donnee[mail] </td>";
+                        echo "<td class='tdstyle whitefonttdstyle'> $donnee[tel] </td>";
+                        echo "<td class='tdstyle whitefonttdstyle'> $donnee[rue] </td>";
+                        echo "<td class='tdstyle whitefonttdstyle'> $donnee[CP] </td>";
+                        echo "<td class='tdstyle whitefonttdstyle'> $donnee[ville] </td>";
+                        echo "<td class='tdstyle whitefonttdstyle'>" . valueGlisse($donnee['glisse']) . "</td>";
+                        echo "<td class='tdstyle whitefonttdstyle'> $donnee[pointure] </td>";
+                        echo "<td class='tdstyle whitefonttdstyle'> $donnee[taille] </td>";
+                        echo "<td class='tdstyle whitefonttdstyle'>" . valueNiveau($donnee['niveau']) . "</td>";
+                        echo "<td style='display: none'>" . valueEtatInscr($donnee['etatInscription']) . "</td>";
+                        echo "<td class='tdstyle whitefonttdstyle'> $donnee[dateInscription]</td>";
+                        echo "<td class='tdstyle whitefonttdstyle'><a class=\"btn btn-success btn-xs\" href=\"acceptUser.php?id=\"" . $donnee['idInscript'] . " role=\"button\">Accept</a>
+                        <a class=\"btn btn-danger btn-xs\" href=\"refuseUser.php?id=\"" . $donnee['idInscript'] . " role=\"button\">Refuser</a>";
+                        echo "</tr>";
+                    }
 
-                } ?>
-            </table>
+                    } ?>
+                </table>
+            </div>
             <h1 class="title">Tous les utilisateurs</h1>
+
+            <button type="button" class="btn btn-info">Tous</button>
+            <button type="button" class="btn btn-success">Validés</button>
+            <button type="button" class="btn btn-danger">Refusés</button>
             <?php
             $data = getAll(); ?>
             <div class="table-responsive">
-            <table class="table table-condensed table-hover">
-                <thead class="entete">
-                <tr>
-                    <th class="thstyle">Nom</th>
-                    <th>Prenom</th>
-                    <th>Date de naissance</th>
-                    <th>Sexe</th>
-                    <th>Email</th>
-                    <th>Telephone</th>
-                    <th>Rue</th>
-                    <th>Code postal</th>
-                    <th>Ville</th>
-                    <th>Ski / Snow</th>
-                    <th>Pointure</th>
-                    <th>Taille</th>
-                    <th>Niveau</th>
-                    <th>Etat de l'inscription</th>
-                    <th>Date d'inscription</th>
-                    <th>Actions</th>
-                </tr>
-                </thead>
-                <?php
-                while ($donnee = $data->fetch()) {
-                    echo " </tr > ";
-                    echo "<td ><input type = 'text' value = '$donnee[nom] ' disabled > </td > ";
-                    echo "<td ><input type = 'text' value = '$donnee[prenom]' disabled > </td > ";
-                    echo "<td ><input type = 'text' value = '$donnee[dateNais]' disabled >  </td > ";
-                    echo "<td ><input type = 'text' value = '" . valueSexe($donnee['sexe']) . "' disabled > </td > ";
-                    echo "<td ><input type = 'text' value = '$donnee[mail] ' disabled > </td > ";
-                    echo "<td ><input type = 'text' value = '$donnee[tel] ' disabled > </td > ";
-                    echo "<td > <input type = 'text' value = '$donnee[rue]' disabled > </td > ";
-                    echo "<td > <input type = 'text' value = '$donnee[CP]' disabled > </td > ";
-                    echo "<td ><input type = 'text' value = '$donnee[ville]' disabled >  </td > ";
-                    echo "<td ><input type = 'text' value = '" . valueGlisse($donnee['glisse']) . "' disabled ></td > ";
-                    echo "<td ><input type = 'text' value = '$donnee[pointure]' disabled >  </td > ";
-                    echo "<td ><input type = 'text' value = '$donnee[taille]' disabled >  </td > ";
-                    echo "<td ><input type = 'text' value = '" . valueNiveau($donnee['niveau']) . "' disabled ></td > ";
-                    echo "<td > " . valueEtatInscr($donnee['etatInscription']) . " </td > ";
-                    echo "<td > $donnee[dateInscription]</td > ";
-                    echo "<td > " . afficheButton($donnee['idInscript'], $donnee['etatInscription']) . " <button type = \"button\" class=\"btn btn-info\">Info</button>
+                <table class="table table-condensed table-hover tablestyle">
+                    <thead class="entete">
+                    <tr>
+                        <th class="thstyle">Nom</th>
+                        <th>Prenom</th>
+                        <th>Date de naissance</th>
+                        <th>Sexe</th>
+                        <th>Email</th>
+                        <th>Telephone</th>
+                        <th>Rue</th>
+                        <th>Code postal</th>
+                        <th>Ville</th>
+                        <th>Ski / Snow</th>
+                        <th>Pointure</th>
+                        <th>Taille</th>
+                        <th>Niveau</th>
+                        <th>Etat de l'inscription</th>
+                        <th>Date d'inscription</th>
+                        <th>Actions</th>
+                    </tr>
+                    </thead>
+                    <?php
+                    while ($donnee = $data->fetch()) {
+                        echo " </tr > ";
+                        echo "<td class='tdstyle' ><input type = 'text' value = '$donnee[nom] ' disabled > </td > ";
+                        echo "<td class='tdstyle'><input type = 'text' value = '$donnee[prenom]' disabled > </td > ";
+                        echo "<td class='tdstyle'><input type = 'text' value = '$donnee[dateNais]' disabled >  </td > ";
+                        echo "<td class='tdstyle'><input type = 'text' value = '" . valueSexe($donnee['sexe']) . "' disabled > </td > ";
+                        echo "<td class='tdstyle'><input type = 'text' value = '$donnee[mail] ' disabled > </td > ";
+                        echo "<td class='tdstyle'><input type = 'text' value = '$donnee[tel] ' disabled > </td > ";
+                        echo "<td class='tdstyle'> <input type = 'text' value = '$donnee[rue]' disabled > </td > ";
+                        echo "<td class='tdstyle'> <input type = 'text' value = '$donnee[CP]' disabled > </td > ";
+                        echo "<td class='tdstyle'><input type = 'text' value = '$donnee[ville]' disabled >  </td > ";
+                        echo "<td class='tdstyle'><input type = 'text' value = '" . valueGlisse($donnee['glisse']) . "' disabled ></td > ";
+                        echo "<td class='tdstyle'><input type = 'text' value = '$donnee[pointure]' disabled >  </td > ";
+                        echo "<td class='tdstyle'><input type = 'text' value = '$donnee[taille]' disabled >  </td > ";
+                        echo "<td class='tdstyle'><input type = 'text' value = '" . valueNiveau($donnee['niveau']) . "' disabled ></td > ";
+                        echo "<td class='tdstyle whitefonttdstyle'> " . valueEtatInscr($donnee['etatInscription']) . " </td > ";
+                        echo "<td class='tdstyle whitefonttdstyle'> $donnee[dateInscription]</td > ";
+                        echo "<td class='tdstyle'> " . afficheButton($donnee['idInscript'], $donnee['etatInscription']) . " <button type = \"button\" class=\"btn btn-info btn-xs\">Info</button>
 
 </td>";
-                    echo "</tr>";
-                }
-                ?>
-            </table>
+                        echo "</tr>";
+                    }
+                    ?>
+                </table>
             </div>
             <?php
 
@@ -151,9 +157,9 @@ include('../head.php');
             function afficheButton($idUser, $etatUser)
             {
                 if ($etatUser == 1) {
-                    return "<a class=\"btn btn-danger\" href=refuseUser.php?id=" . $idUser . " role=\"button\">Refuser</a>";
+                    return "<a class=\"btn btn-danger btn-xs\" href=refuseUser.php?id=" . $idUser . " role=\"button\">Refuser</a>";
                 } else {
-                    return "<a class=\"btn btn-success\" href=acceptUser.php?id=" . $idUser . " role=\"button\">Accept</a>";
+                    return "<a class=\"btn btn-success btn-xs\" href=acceptUser.php?id=" . $idUser . " role=\"button\">Accept</a>";
 
                 }
             }
