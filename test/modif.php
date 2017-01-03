4b51a1e0f8491219a10f649cf341e150fa4d389d
Maeva <maeva.mgt@hotmail.fr>
2017-01-03 12:58:00 +0100

    modifier

--- test/modif.php
+++ test/modif.php
@@ -1,4 +1,252 @@
 <?php
 require('../bdd.php');
-newInsc($_GET['idInscript'],$_GET['nom'], $_GET['prenom'], $date, $_GET['sexe'], $_GET['mail'], $_GET['tel'], $_GET['rue'], $_GET['CP'], $_GET['ville'], $_GET['glisse'], $_GET['pointure'], $_GET['taille'], $_GET['niveau'], $_GET['etatInscription'], $_GET['dateInscription']);
-//header('Location: admin.php');
+include('../head.php');
+
+$data =getUserById($_GET['id']);
+$valeur = 
+
+//modifyUser($_GET['idInscript'],$_GET['nom'], $_GET['prenom'], $date, $_GET['sexe'], $_GET['mail'], $_GET['tel'], $_GET['rue'], $_GET['CP'], $_GET['ville'], $_GET['glisse'], $_GET['pointure'], $_GET['taille'], $_GET['niveau'], $_GET['etatInscription'], $_GET['dateInscription']);
+?>
+<!-- CSS -->
+<link rel="stylesheet" href="admin.css" media="all">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--Icon -->
+<link rel="icon" href="../images/snowflakeicon.png">
+
+<body>
+<div class="container-fluid background-home">
+    <div class="row">
+        <div class="col-xs-12">
+            <h1>Modification d'un utilisateur</h1>
+            <div class="well center-block" style="max-width:600px">
+                <!-- Formulaire -->
+                <form action="" method="get">
+                    <!— Nom —>
+                    <div class="col-xs-12">
+                        <div class="form-group">
+                            <label class="intform" for="nom">Ton Nom :</label>
+                            <?php echo "<input type='text' class='form-control' id='nom' name='nom' value='.$valeur[nom].'>"; ?>
+                        </div>
+                    </div>
+
+                    <!— Prenom —>
+                    <div class="col-xs-12">
+                        <div class="form-group">
+                            <label class="intform" for="prenom">Prenom :</label>
+                            <input type="text" class="form-control" id="prenom" name="prenom">
+                        </div>
+                    </div>
+
+                    <!— Date naissance —>
+                    <div class="col-xs-12">
+                        <label class="intform">Date de naissance :</label>
+                        <!-- Jour de naissance -->
+                        <div class="form-inline" style="margin-top: 10px; margin-left: 15px; margin-bottom: 15px">
+                            <label class="intform" for=""> Jour :</label>
+                            <select class="form-control" id="" name="jour">
+                                <option></option>
+                                <option value="01">01</option>
+                                <option value="02">02</option>
+                                <option value="03">03</option>
+                                <option value="04">04</option>
+                                <option value="05">05</option>
+                                <option value="06">06</option>
+                                <option value="07">07</option>
+                                <option value="08">08</option>
+                                <option value="09">09</option>
+                                <option value="10">10</option>
+                                <option value="11">11</option>
+                                <option value="12">12</option>
+                                <option value="13">13</option>
+                                <option value="14">14</option>
+                                <option value="15">15</option>
+                                <option value="16">16</option>
+                                <option value="17">17</option>
+                                <option value="18">18</option>
+                                <option value="19">19</option>
+                                <option value="20">20</option>
+                                <option value="21">21</option>
+                                <option value="22">22</option>
+                                <option value="23">23</option>
+                                <option value="24">24</option>
+                                <option value="25">25</option>
+                                <option value="26">26</option>
+                                <option value="27">27</option>
+                                <option value="28">28</option>
+                                <option value="29">29</option>
+                                <option value="30">30</option>
+                                <option value="31">31</option>
+                            </select>
+                            <!-- Mois de naissance -->
+                            <label class="intform" style="margin-left: 15px">Mois:</label>
+                            <select class="form-control" id="" name="mois">
+                                <option></option>
+                                <option value="01">01</option>
+                                <option value="02">02</option>
+                                <option value="03">03</option>
+                                <option value="04">04</option>
+                                <option value="05">05</option>
+                                <option value="06">06</option>
+                                <option value="07">07</option>
+                                <option value="08">08</option>
+                                <option value="09">09</option>
+                                <option value="10">10</option>
+                                <option value="11">11</option>
+                                <option value="12">12</option>
+                            </select>
+                            <!-- Année de naissance -->
+                            <label class="intform" style="margin-left: 15px">Annee:</label>
+                            <select class="form-control" id="" name="annee">
+                                <option></option>
+                                <option value="1998">1998</option>
+                                <option value="1997">1997</option>
+                                <option value="1996">1996</option>
+                                <option value="1995">1995</option>
+                                <option value="1994">1994</option>
+                                <option value="1993">1993</option>
+                                <option value="1992">1992</option>
+                                <option value="1991">1991</option>
+                                <option value="1990">1990</option>
+                                <option value="1989">1989</option>
+                                <option value="1987">1987</option>
+                                <option value="1986">1986</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <!— Sexe —>
+                    <div class="col-xs-12">
+                        <div class="form-group">
+                            <label class="intform">Sexe :</label>
+                            <div class="radio">
+                                <label><input type="radio" name="sexe" value="0" class="intform">Homme</label>
+                            </div>
+                            <div class="radio">
+                                <label><input type="radio" name="sexe" value="1">Femme</label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!— Mail —>
+                    <div class="col-xs-12">
+                        <div class="form-group">
+                            <label class="intform" for="mail">Mail :</label>
+                            <input type="email" class="form-control" id="mail" name="mail">
+                        </div>
+                    </div>
+
+                    <!— Tel —>
+                    <div class="col-xs-12">
+                        <div class="form-group">
+                            <label class="intform" for="tel">Telephone :</label>
+                            <input type="tel" class="form-control" id="tel" name="tel">
+                        </div>
+                    </div>
+
+                    <!— Addresse —>
+                    <!— Rue-->
+                    <div class="col-xs-12">
+                        <div class="form-group">
+                            <label class="intform" for="adresse">Adresse :</label>
+                            <input type="text" class="form-control" id="adresse" name="rue">
+                        </div>
+                    </div>
+                    <!— CP-->
+                    <div class="col-xs-12">
+                        <div class="form-group">
+                            <label class="intform" for="cp">Code postale :</label>
+                            <input type="text" class="form-control" id="cp" name="cp">
+                        </div>
+                    </div>
+                    <!— Ville-->
+                    <div class="col-xs-12">
+                        <div class="form-group">
+                            <label class="intform" for="ville">Ville :</label>
+                            <input type="text" class="form-control" id="ville" name="ville">
+                        </div>
+                    </div>
+
+                    <!— Ski / Snow —>
+                    <div class="col-xs-12">
+                        <div class="form-group">
+                            <label class="intform">Ski ou snow ?</label>
+                            <div class="radio">
+                                <label><input type="radio" name="glisse" value="0">Ski</label>
+                            </div>
+                            <div class="radio">
+                                <label><input type="radio" name="glisse" value="1">Snow</label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!— Pointure —>
+                    <div class="form-group">
+                        <label class="intform" for="pointure">Pointure :</label>
+                        <select class="form-control" id="pointure" name="pointure">
+                            <option></option>
+                            <option value="33">33</option>
+                            <option value="34">34</option>
+                            <option value="35">35</option>
+                            <option value="36">36</option>
+                            <option value="37">37</option>
+                            <option value="38">38</option>
+                            <option value="39">39</option>
+                            <option value="40">40</option>
+                            <option value="41">41</option>
+                            <option value="42">42</option>
+                            <option value="43">43</option>
+                            <option value="44">44</option>
+                            <option value="45">45</option>
+                            <option value="46">46</option>
+                        </select>
+                    </div>
+
+                    <!— taille-->
+                    <div class="form-group">
+                        <label class="intform" for="taille">Taille :</label>
+                        <select class="form-control" id="taille" name="taille">
+                            <option></option>
+                            <option value="155">155cm</option>
+                            <option value="160">160cm</option>
+                            <option value="165">165cm</option>
+                            <option value="170">170cm</option>
+                            <option value="175">175cm</option>
+                            <option value="180">180cm</option>
+                            <option value="185">185cm</option>
+                            <option value="190">190cm</option>
+                            <option value="195">195cm</option>
+                            <option value="200">200cm</option>
+                        </select>
+                    </div>
+
+                    <!— Niveau —>
+                    <div class="col-xs-12">
+                        <div class="form-group">
+                            <label class="intform">Niveau :</label>
+                            <div class="radio">
+                                <label><input type="radio" name="niveau" value="0">Débutant</label>
+                            </div>
+                            <div class="radio">
+                                <label><input type="radio" name="niveau" value="1">Intermédiaire</label>
+                            </div>
+                            <div class="radio">
+                                <label><input type="radio" name="niveau" value="2">Expert</label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!— Validation Formulaire —>
+                    <p>NB: Tous les champs sont obligatoires !</p>
+                    <div class="btncenter style-bottom">
+                        <a class="btn btn-info" href="admin.php" role="button">Retour</a>
+                        <input class="btn btn-success" type="submit" name="submit" value="Envoyer !">
+                    </div>
+                </form>
+            </div>
+
+        </div>
+        <!-- fin row -->
+    </div>
+    <!-- fin container -->
+</div>
+</body>
